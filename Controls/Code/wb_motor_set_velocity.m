5dacdbc19d0a396d06ae0df5fb48d6eb5513a59d
LouisNguyen25 <70172238+LouisNguyen25@users.noreply.github.com>
2021-11-05 16:50:03 -0700

    Create wb_motor_set_velocity.m

---
+++ Controls/Code/wb_motor_set_velocity.m
@@ -0,0 +1,7 @@
+function wb_motor_set_velocity(tag, velocity)
+% Usage: wb_motor_set_velocity(tag, velocity)
+% Matlab API for Webots
+% Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/motor">here</a>
+
+coder.extrinsic('calllib');
+calllib('libController', 'wb_motor_set_velocity', tag, velocity);
